09e3d687898075931485179867c2f64646ecf4a3
Tim Eapen <eapent@intware.com>
2016-03-28 21:26:59 -0400

    Adding spec test for main controller

--- pet-client/src/app/main/main.controller.spec.js
+++ pet-client/src/app/main/main.controller.spec.js
@@ -2,38 +2,31 @@
   'use strict';
 
   describe('controllers', function(){
+    var $controller;
     var vm;
-    var $timeout;
-    var toastr;
+    var loginService;
 
     beforeEach(module('petClient'));
-    beforeEach(inject(function(_$controller_, _$timeout_, _webDevTec_, _toastr_) {
-      spyOn(_webDevTec_, 'getTec').and.returnValue([{}, {}, {}, {}, {}]);
-      spyOn(_toastr_, 'info').and.callThrough();
-
-      vm = _$controller_('MainController');
-      $timeout = _$timeout_;
-      toastr = _toastr_;
+    beforeEach(inject(function(_$controller_, _$rootScope_, _loginService_) {
+      $controller = _$controller_;
+      loginService = _loginService_;
     }));
 
-    it('should have a timestamp creation date', function() {
-      expect(vm.creationDate).toEqual(jasmine.any(Number));
+    it('should attempt login on controller creation', function() {
+      spyOn(loginService, 'login');
+      vm = $controller('MainController');
+      expect(loginService.login).toHaveBeenCalledWith(undefined, jasmine.any(Function));
     });
 
-    it('should define animate class after delaying timeout ', function() {
-      $timeout.flush();
-      expect(vm.classAnimation).toEqual('rubberBand');
+    it('should login', function() {
+      vm = $controller('MainController');
+
+      spyOn(loginService, 'login').and.callThrough();
+      vm.credentials = {"name" : "Sandhra", "password" : "Baby"};
+      vm.login();
+
+      expect(loginService.login).toHaveBeenCalledWith({"name" : "Sandhra", "password" : "Baby"}, jasmine.any(Function));
     });
 
-    it('should show a Toastr info and stop animation when invoke showToastr()', function() {
-      vm.showToastr();
-      expect(toastr.info).toHaveBeenCalled();
-      expect(vm.classAnimation).toEqual('');
-    });
-
-    it('should define more than 5 awesome things', function() {
-      expect(angular.isArray(vm.awesomeThings)).toBeTruthy();
-      expect(vm.awesomeThings.length === 5).toBeTruthy();
-    });
   });
 })();
