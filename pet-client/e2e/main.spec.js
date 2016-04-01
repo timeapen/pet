d1ee172d54ea693ffe11a84f5b97c87907890c16
Tim Eapen <eapent@intware.com>
2016-03-31 20:31:45 -0400

    Tweaking login page styling. Adding more e2e

--- pet-client/e2e/main.spec.js
+++ pet-client/e2e/main.spec.js
@@ -2,29 +2,38 @@
 
 describe('The main view', function () {
   var page;
-  var petAdd;
+  var addPage;
 
   beforeEach(function () {
-    browser.get('/index.html');
     page = require('./main.po');
-    petAdd = require('./petadd.po');
+    addPage = require('./petadd.po');
   });
 
   it('should login and forward to Add Pet page', function() {
+    browser.get('/index.html');
     page.username.sendKeys('santo');
     page.password.sendKeys('baby');
     page.login.click();
-    expect(petAdd.pageHeading.getText()).toBe('Add Pet');
+
+    expect(addPage.pageHeading.getText()).toBe('Add Pet');
   });
 
-  it('should include jumbotron with correct data', function() {
-    expect(page.h1El.getText()).toBe('\'Allo, \'Allo!');
-    expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
-    expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
+  it('should add pet', function() {
+    addPage.enterName('Blackie');
+    addPage.enterDescription('A german shepherd');
+    addPage.clickAdd();
+
+    expect(addPage.successMessage.getText()).toMatch(/Added pet with id: \d+/);
   });
 
-  it('should list more than 5 awesome things', function () {
-    expect(page.thumbnailEls.count()).toBeGreaterThan(5);
-  });
+  //it('should include jumbotron with correct data', function() {
+  //  expect(page.h1El.getText()).toBe('\'Allo, \'Allo!');
+  //  expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
+  //  expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
+  //});
+  //
+  //it('should list more than 5 awesome things', function () {
+  //  expect(page.thumbnailEls.count()).toBeGreaterThan(5);
+  //});
 
 });
--- pet-client/e2e/petadd.po.js
+++ pet-client/e2e/petadd.po.js
@@ -7,6 +7,23 @@
 
 var PetAddPage = function() {
   this.pageHeading = element(by.css('h1'));
+  this.name = element(by.id('name'));
+  this.description = element(by.id('description'));
+  this.add = element(by.id('add'));
+  this.successMessage = element(by.id('addSuccess'));
+
+  this.enterName = function(name) {
+    this.name.sendKeys(name);
+  }
+
+  this.enterDescription = function(description) {
+    this.description.sendKeys(description);
+  }
+
+  this.clickAdd = function() {
+    this.add.click();
+  }
+
 };
 
 module.exports = new PetAddPage();
